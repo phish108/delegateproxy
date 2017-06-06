506f679c8dba0dd8485d145c2e47e5a1be039560
Christian Glahn <cpglahn@gmail.com>
2017-06-07 00:11:20 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,207 @@
+# DelegateProxy - ES6 Proxy Class for Functional Delegation
+
+DelegateProxy uses the ES6 Proxies for linking two objects.
+
+DelegateProxy is a utility factory for implementing class delegation. It allows
+to implement class inheritance and code encapsulation without bothering with
+class syntax.
+
+DelegateProxy binds two objects functionally, so both objects can access the
+same functions and data as if they were a single instance. DelegateProxy allows
+programmers to capsulate functions and data without creating complex class
+hierarchies.
+
+DelegateProxy started from an attempt for applying the ES6 Meta-programming
+features in the real world. It turned out to be a useful tool for avoiding
+class inheritance and implementing plugable features.
+
+## Basic syntax
+
+```javascript
+const proxy = DelegateProxy(OperatorObject, DelegateObject);
+```
+
+## How to use
+
+The most basic use for DelegateProxy is to link to classes.
+
+```javascript
+class A {
+    a() {
+        return "hello from A";
+    }
+}
+
+class B {
+    b() {
+        return "hello from B";
+    }
+}
+
+const dp = DelegateProxy(A, B);
+
+// now we can use functions from A and B
+console.log(dp.a()); // "hello from A"
+console.log(dp.b()); // "hello from B"
+```
+
+DelegateProxy will instantiate both classes and then tie them together.
+
+Within a delegate proxy, the operator object is write only. Data can be stored
+only in the delegate object.
+
+```javascript
+class A {
+    constructor() {
+        this.aData = "immutable";
+    }
+
+    a() {
+        return this.aData;
+    }
+}
+
+class B {
+    constructor() {
+        this.bData = "mutable";
+    }
+
+    mutate() {
+        this.aData = "mutate"; // will fail in proxy
+    }
+
+    b() {
+        return `${this.aData} ${this.bData}`;
+    }
+}
+
+const dp = DelegateProxy(A, B);
+
+dp.mutate(); // will fail by throwing an error
+console.log(dp.b()); // "immutable mutable"
+```
+
+All properties of the operator object have preference over those in
+the delegate. This means that properties of the delegate cannot override the
+properties of the operator object.
+
+```javascript
+class A {
+    constructor() {
+        this.aData = "Hello World";
+    }
+}
+
+class B {
+    constructor() {
+        this.aData = "My Data";
+    }
+
+    b() {
+        return this.aData;
+    }
+}
+
+const dp = DelegateProxy(A, B);
+
+console.log(dp.b()); // "Hello World"
+```
+
+If both classes define the same function, they are called in the order of the
+delegation.
+
+```javascript
+class A {
+    a() {
+        this.data = "Hello ";
+    }
+}
+
+class B {
+    a() {
+        this.data += "World";
+    }
+    b() {
+        return this.data;
+    }
+}
+
+const dp = DelegateProxy(A, B);
+
+dp.a();
+console.log(dp.b()); // "Hello World"
+```
+
+It is possible to pass objects directly into the delegation, both as operators
+and as delegates.
+
+Of course, delegations can be chained. Therefore, the delegate method is
+reserved to DelegateProxy.
+
+```javascript
+const writeOnly = {
+    fixed: "immutable"
+};
+
+class A {
+    a() {
+        this.changed = "Hello ";
+    }
+}
+
+class B {
+    a() {
+        this.changed += "World";
+    }
+}
+
+const dp1 = DelegateProxy(writeOnly, A).delegate(B);
+// same as
+// const dp2 = DelegateProxy(DelegateProxy(writeOnly, A), B)
+
+dp1.a();
+console.log(dp1.fixed);   // "immutable"
+console.log(dp1.changed); // "Hello World"
+```
+
+Because DelegateProxy does not alter the objects that are passed into it,
+this allows to implement class inheritance based on singletons.
+
+```javascript
+class A {
+    a() {
+        this.changed = "Hello ";
+    }
+}
+
+class B {
+    a() {
+        this.changed += "World";
+    }
+}
+
+class C {
+    a() {
+        this.changed += "Mars";
+    }
+}
+
+const base = new A();
+const dp1 = DelegateProxy(base, B);
+const dp2 = DelegateProxy(base, C);
+
+dp1.a();
+dp2.a();
+
+console.log(base.changed); // undefined
+console.log(dp1.changed);  // "Hello World"
+console.log(dp2.changed);  // "Hello Mars"
+```
+
+## AUTHOR
+
+Christian Glahn (https://github.com/phish108)
+
+## License
+
+MIT
---
+++ test/01_delegateproxy.test.js
@@ -0,0 +1,204 @@
+"use strict";
+
+const { expect } = require("chai");
+const DelegateProxy = require("../nodejs/DelegateProxy");
+
+describe("DelegateProxy", function() {
+    it("load", function () {
+        expect(DelegateProxy).to.be.a("function");
+    });
+
+    it("simple proxy", function () {
+        const a = {
+            foo: "hello"
+        };
+        const b = {
+            bar: "world"
+        };
+        const result = DelegateProxy(a, b);
+
+        expect(result).is.not.equal(a);
+        expect(result.foo).to.be.equal("hello");
+        expect(result.bar).to.be.equal("world");
+    });
+
+    it("write only base proxy", function() {
+        const a = {
+            foo: "hello"
+        };
+        const b = {
+            bar: "world"
+        };
+        const result = DelegateProxy(a, b);
+
+        expect(result).is.not.equal(a);
+        try {
+            result.foo = "Hello";
+        }
+        catch (err) {
+            // skip
+        }
+        try {
+            result.bar = "World";
+        }
+        catch (err) {
+            // skip again
+        }
+        expect(result.foo).to.be.equal("hello");
+        expect(result.bar).to.be.equal("World");
+    });
+
+    it("auto instantiate left", function() {
+        class A {
+            constructor() {
+                this.foo = "Hello ";
+            }
+        }
+        const b = {
+            bar: "World"
+        };
+
+        const result = DelegateProxy(A, b);
+
+        expect(result.foo).to.be.equal("Hello ");
+        expect(result.bar).to.be.equal("World");
+    });
+
+    it("auto instantiate right", function() {
+        class A {
+            constructor() {
+                this.foo = "Hello ";
+            }
+        }
+        const b = {
+            bar: "World"
+        };
+
+        const result = DelegateProxy(b, A);
+
+        expect(result.foo).to.be.equal("Hello ");
+        expect(result.bar).to.be.equal("World");
+    });
+
+    it("function cascade", function() {
+        class A {
+            a() {
+                this.data = "Hello ";
+            }
+        }
+
+        class B {
+            a() {
+                this.data += "World";
+            }
+        }
+
+        const result = DelegateProxy(A, B);
+
+        result.a();
+        expect(result.data).to.be.equal("Hello World");
+    });
+
+    it("delegation cascade", function() {
+        class A {
+            a() {
+                this.data = "Hello ";
+            }
+        }
+
+        class B {
+            a() {
+                this.data += "World ";
+            }
+        }
+
+        class C {
+            a() {
+                this.data += "Class ";
+            }
+        }
+
+        const result = DelegateProxy(A, B).delegate(C);
+
+        result.a();
+        expect(result.data).to.be.equal("Hello World Class ");
+    });
+
+    it("delegation cascade reserved", function() {
+        class A {
+            delegate(cls) {
+                this.data = "not called";
+            }
+        }
+
+        class B {
+            a() {
+                this.foo = "hello ";
+            }
+        }
+
+        class C {
+            a() {
+                this.foo += "world ";
+            }
+        }
+
+        const result = DelegateProxy(A, B).delegate(C);
+
+        expect(result.data).to.be.undefined;
+    });
+
+    it("singleton delegation base", function() {
+        class Single {
+            a(value) {
+                this.data = `hello ${value}`;
+            }
+        }
+
+        const base = new Single();
+        const d1   = DelegateProxy(base, {});
+        const d2   = DelegateProxy(base, {});
+
+        d1.a("world");
+        d2.a("mars");
+
+        expect(d1.data).to.be.equal("hello world");
+        expect(d2.data).to.be.equal("hello mars");
+    });
+
+    it("singleton delegation both", function() {
+        class Single {
+            a(value) {
+                this.data = `hello ${value}`;
+            }
+        }
+
+        const base = new Single();
+        const data = {};
+        const d1   = DelegateProxy(base, data);
+
+        d1.a("world");
+
+        expect(d1.data).to.be.equal("hello world");
+        expect(data.data).to.be.equal("hello world");
+    });
+
+    it("singleton delegation change base", function() {
+        class Single {
+            a(value) {
+                this.data = `hello ${value}`;
+            }
+        }
+
+        const base = new Single();
+        const data = {};
+        const d1   = DelegateProxy(base, data);
+
+        base.hello = "foo";
+        d1.a("world");
+
+        expect(d1.data).to.be.equal("hello world");
+        expect(data.data).to.be.equal("hello world");
+        expect(d1.hello).to.be.equal("foo");
+    });
+});
