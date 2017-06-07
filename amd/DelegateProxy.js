7fa6e3bd7d05817df2f5192cf5e01297a2b1d815
Christian Glahn <cpglahn@gmail.com>
2017-06-07 09:29:50 +0200

    automatically create amd and nodejs code from ES6 source

--- amd/DelegateProxy.js
+++ amd/DelegateProxy.js
@@ -1,33 +1,4 @@
-/**
- * This file is part of delegateproxy
- *
- * MIT License
- *
- * Copyright 2017 Christian Glahn (github.com/phish108)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
-
- /* eslint-env es6, amd */
-
-define( "DelegateProxy", function () {
-
+define("DelegateProxy", [], function() {
     return function DelegateProxy(operator, delegate) {
         if (typeof operator === "function") {
             operator = new operator();
@@ -35,48 +6,39 @@ define( "DelegateProxy", function () {
         if (typeof delegate === "function") {
             delegate = new delegate();
         }
-
         const proxy = {};
-
         return new Proxy(operator, {
             construct: function(o, args, p) {
                 return p;
             },
-
-            set: function(o,p,v) {
+            set: function(o, p, v) {
                 if (!o[p]) {
                     delegate[p] = v;
                     return true;
                 }
                 return false;
             },
-
-            get: function(o,p) {
+            get: function(o, p) {
                 if (p === "delegate") {
                     return function chainDelegate(proxyObject) {
                         return DelegateProxy(this, proxyObject);
                     };
                 }
-
-                if (typeof operator[p] === "function" &&
-                    typeof delegate[p] === "function") {
+                if (typeof operator[p] === "function" && typeof delegate[p] === "function") {
                     if (!proxy[p]) {
-                        proxy[p] = function (...args) {
-                            let result = delegate[p].apply(this, args);
-
+                        proxy[p] = function(...args) {
+                            let result = operator[p].apply(this, args);
                             if (!result || !result.length) {
-                                result = operator[p].apply(this, args);
+                                result = delegate[p].apply(this, args);
                             }
                             return result;
                         };
                     }
                     return proxy[p];
                 }
-
                 if (operator[p]) {
                     return operator[p];
                 }
-
                 return delegate[p];
             }
         });
